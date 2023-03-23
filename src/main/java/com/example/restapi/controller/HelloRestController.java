ed41bf2d9185e4f1b0a56ec510a00d05e9437a0f
shiwani <shiwanipaunikar1@gmail.com>
2023-03-23 05:31:39 -0700

    Added:Rest Api call program

---
+++ src/main/java/com/example/restapi/controller/HelloRestController.java
@@ -0,0 +1,12 @@
+package com.example.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class HelloRestController {
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello From BridgeLabz!!!";
+        }
+}
